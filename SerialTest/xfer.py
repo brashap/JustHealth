ecdc805d5e139ef6002804dcbe5ad01effef5c46
rashap <brashap@cnm.edu>
2021-05-28 14:04:21 -0600

    Serial Test

---
+++ SerialTest/xfer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import time
+import serial
+
+print("Starting Receiver Program")
+
+serial_port = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+)
+# Wait a second to let the port initialize
+time.sleep(1)
+
+try:
+    # Send a simple header
+    serial_port.write("UART Demonstration Program\r\n".encode())
+    serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
+    while True:
+        i = 0
+        data = b""
+        while i < 176:
+            if serial_port.inWaiting() > 0:
+                data = data + serial_port.read()
+                i = i + 1
+                #print(data)
+                #serial_port.write(data)
+                # if we get a carriage return, add a line feed too
+                # \r is a carriage return; \n is a line feed
+                # This is to help the tty program on the other end 
+                # Windows is \r\n for carriage return, line feed
+                # Macintosh and Linux use \n
+                if data == "\r".encode():
+                    # For Windows boxen on the other end
+                    serial_port.write("\n".encode())
+        print(data)
+
+
+except KeyboardInterrupt:
+    print("Exiting Program")
+
+except Exception as exception_error:
+    print("Error occurred. Exiting Program")
+    print("Error: " + str(exception_error))
+
+finally:
+    serial_port.close()
+    pass
